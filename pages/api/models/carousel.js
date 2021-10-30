a4f3168d078724f815f82c7c949a2ec6498d5dbf
aashish-cd <076bct002@ioepc.edu.np>
2021-10-30 18:36:01 +0545

    carousel improvement

--- pages/api/models/carousel.js
+++ pages/api/models/carousel.js
@@ -1,33 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import { Zoom } from 'react-slideshow-image';
-import Image from 'next/image';
+import style from './carousel.module.css';
 import 'react-slideshow-image/dist/styles.css';
 
 import data from '../carousel';
+
 const CarouselImage = () => {
+  const slideRef = useRef();
   const [cr, setCr] = useState(data);
   const zoomInProperties = {
     indicators: true,
+    indicators: (i) => (
+      <div className={style.indicator}>
+        <img
+          src={`/carousel/${i + 1}.jpg`}
+          alt='chitwan kagaz'
+          style={{ height: '40px' }}
+        />
+      </div>
+    ),
     scale: 1.4,
     canSwipe: true,
-    duration: 1500,
-    transitionDuration: 500,
-    pauseOnHover: false,
+    duration: 2000,
+    transitionDuration: 800,
+    pauseOnHover: true,
+  };
+  const goto = (id) => {
+    slideRef.current.goTo(id);
   };
   console.log(data);
   return (
-    <div className='content-center self-center pt-5'>
-      <Zoom {...zoomInProperties}>
+    <div className='content-center self-center pt-5 '>
+      <Zoom {...zoomInProperties} ref={slideRef}>
         {cr.map((each, index) => (
-          <div key={index} style={{ width: '100%' }}>
+          <>
+            <div key={index} style={{ width: '100%' }}>
+              <img
+                style={{
+                  objectFit: 'cover',
+                  width: '100%',
+                  maxHeight: '500px',
+                }}
+                src={`/carousel/${each.id}.jpg`}
+              />
+              <div className='absolute text-lg -mt-16 text-white px-4'>
+                <p>{each.name}</p>
+              </div>
+            </div>
+          </>
+        ))}
+      </Zoom>
+      {/* <div className={style.container}>
+        {cr.map((ss) => (
+          <div key={ss.id}>
             <img
-              style={{ objectFit: 'cover', width: '100%', maxHeight: '500px' }}
-              src={`/carousel/${each.id}.jpg`}
+              src={`/carousel/${ss.id}.jpg`}
+              alt={ss.name}
+              onClick={goto(ss.id)}
             />
           </div>
         ))}
-      </Zoom>
+      </div> */}
     </div>
   );
 };
--- pages/products.js
+++ pages/products.js
@@ -7,7 +7,7 @@ const Products = () => {
       <h1 className='text-center title-font  sm:text-4xl text-3xl mb-4 font-medium text-gray-900'>
         Our Products
       </h1>
-      <div className='content-center'>
+      <div className='content-center' style={{ padding: '0 2rem' }}>
         <CarouselImage />
       </div>
     </>
