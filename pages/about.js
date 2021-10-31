7b0cd224abc1401d0d802bee8e6b643eebf8b2f3
aashish-cd <076bct002@ioepc.edu.np>
2021-10-31 19:17:22 +0545

    lets c

--- pages/about.js
+++ pages/about.js
@@ -7,21 +7,21 @@ const About = () => {
         About us
       </h1>
 
-      <section class='text-gray-600 body-font'>
-        <div class='container mx-auto flex flex-col px-5 py-24 justify-center items-center'>
-          <div class='w-full md:w-2/3 flex flex-col mb-16 items-center text-center'>
-            <h1 class='title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900'>
+      <section className='text-gray-600 body-font'>
+        <div className='container mx-auto flex flex-col px-5 py-24 justify-center items-center'>
+          <div className='w-full md:w-2/3 flex flex-col mb-16 items-center text-center'>
+            <h1 className='title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900'>
               CKG means Chitwan Kagaz Ghar. The wholeseller of weeding cards &
               printing materials.
             </h1>
-            {/* <p class='mb-8 leading-relaxed'>
+            {/* <p className='mb-8 leading-relaxed'>
               Kickstarter biodiesel roof party wayfarers cold-pressed. Palo
               santo live-edge tumeric scenester copper mug flexitarian. Prism
               vice offal plaid everyday carry. Gluten-free chia VHS squid
               listicle artisan.
             </p> */}
 
-            <p class='text-lg mt-2 text-gray-500 mb-1 w-full'>
+            <p className='text-lg mt-2 text-gray-500 mb-1 w-full'>
               Neutra shabby chic ramps, viral fixie.
             </p>
           </div>
