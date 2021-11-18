af7d846ec73d5f2ab6d19b6dd2f534a7bea85e2d
aashish-cd <076bct002@ioepc.edu.np>
2021-11-19 00:24:39 +0545

    will work later after css

--- pages/api/models/Hero.js
+++ pages/api/models/Hero.js
@@ -29,8 +29,7 @@ const Hero = () => {
                   {service[0].linkName}
                 </button>
               </a>
-            </div>
-            <div className='flex justify-center'>
+
               <a href={service[1].link} className='mx-1'>
                 <button className='inline-flex text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg'>
                   {service[1].linkName}
