63b69115d32040b793acd0a98519171b78f4ee30
aashish-cd <076bct002@ioepc.edu.np>
2021-11-23 15:21:35 +0545

    f*king error

--- pages/_app.js
+++ pages/_app.js
@@ -27,7 +27,7 @@ function MyApp({ Component, pageProps, router }) {
           content='wedding card, offset printing material, screen printing material, bending material, file and copy udhyog material, painting material, stamp material, mini machinery for printing'
         />
         <meta name='robots' content='index, follow' />
-        <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+        <meta httpEquiv='Content-Type' content='text/html; charset=utf-8' />
         <meta name='language' content='English' />
         <link rel='icon' href='/favicon.ico' />
         <link
