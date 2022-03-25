1cb9e932cb23fe3cef527606677f601822885745
Sarthak Parajuli <46929074+kingace2056@users.noreply.github.com>
2022-03-25 10:14:29 +0545

    proprietor photo changed or say fixed

--- pages/api/carousel.js
+++ pages/api/carousel.js
@@ -56,7 +56,7 @@ export const about = [
     postBy: 'Prabhat Dhungana',
     org: 'Proprietor',
     image:
-      'https://scontent.fbir1-1.fna.fbcdn.net/v/t39.30808-6/221920147_4437290126302006_4483589381469953030_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeEBALNQkqIOMeqEFDl0mSsUNpyYlHhwvf42nJiUeHC9_lX6CE7mREKYGEXf5LP_MMPkkFKSASP13YaqQwkKHenQ&_nc_ohc=kfJwAoGVWZkAX-qDMhX&_nc_ht=scontent.fbir1-1.fna&oh=14f885b30bf18450369e8989af5e90db&oe=61A17658',
+      'https://scontent.fbir1-1.fna.fbcdn.net/v/t39.30808-6/221920147_4437290126302006_4483589381469953030_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=aXM24UL1XCwAX_Sav_X&_nc_ht=scontent.fbir1-1.fna&oh=00_AT8YoUtS7_9QL1acIyutnC9WTKTeJZbh5AEcsZTyzdf5LA&oe=6241A498',
     location: 'Press marga, narayanghar, chitwan',
   },
 ];
